cb0d42f8bead8d50a521901b8ef41d74741bd1c9
GABRIEL FERNANDES DE OLIVEIRA <24.01414-0@maua.br>
2024-10-31 09:11:12 -0300

    v8

--- front/JS/frontend.js
+++ front/JS/frontend.js
@@ -66,7 +66,7 @@ async function cadastrarFilme() {
 
         exibirFilmes(filmes)
 
-        exibirAlerta('.alert-filme', 'Filme cadastrado com sucesso', ['show', 'alert-success'], ['d-none'], 2000)
+        exibirAlerta('.alert-filme', 'Filme cadastrado com sucesso', ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
 
     }
     //  senão, exibe o alerta por até 2 segundos
@@ -75,27 +75,58 @@ async function cadastrarFilme() {
     }
 }
 
-async function cadastrarUsuario(){
+async function cadastrarUsuario() {
     let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
     let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
     let usuarioCadastro = usuarioCadastroInput.value
     let passwordCadastro = passwordCadastroInput.value
     if (usuarioCadastro && passwordCadastro) {
-      try{
-      const cadastroEndpoint = '/signup'
-      const URLCompleta = `${protocolo}${baseURL}${cadastroEndpoint}`
-        await axios.post(URLCompleta, { login: usuarioCadastro, password: passwordCadastro })
-        usuarioCadastroInput.value = ""
-        passwordCadastroInput.value = ""
-        exibirAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso!", ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
-        ocultarModal('#modalLogin', 2000)
-      }
-      catch(error){
-        exibirAlerta('.alert-modal-cadastro', "Erro ao cadastrar usuário", ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
-        ocultarModal('#modalLogin', 2000)
-      }
+        try {
+            const cadastroEndpoint = '/signup'
+            const URLCompleta = `${protocolo}${baseURL}${cadastroEndpoint}`
+            await axios.post(URLCompleta, { login: usuarioCadastro, password: passwordCadastro })
+            usuarioCadastroInput.value = ""
+            passwordCadastroInput.value = ""
+            exibirAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso!", ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+            ocultarModal('#modalCadastro', 2000)
+        }
+        catch (error) {
+            exibirAlerta('.alert-modal-cadastro', "Erro ao cadastrar usuário", ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
+            ocultarModal('#modalCadastro', 2000)
+        }
     }
-    else{
-      exibirAlerta('.alert-modal-cadastro', 'Preencha todos os campos', ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
+    else {
+        exibirAlerta('.alert-modal-cadastro', 'Preencha todos os campos', ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
     }
-  }
+}
+
+const fazerLogin = async () => {
+    let usuarioLoginInput = document.querySelector('#usuarioLoginInput')
+    let passwordLoginInput = document.querySelector('#passwordLoginInput')
+    let usuarioLogin = usuarioLoginInput.value
+    let passwordLogin = passwordLoginInput.value
+    if (usuarioLogin && passwordLogin) {
+        try {
+            const loginEndpoint = '/login'
+            const URLCompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post(URLCompleta, { login: usuarioLogin, password: passwordLogin })
+            console.log(response.data)
+            usuarioLoginInput.value = ""
+            passwordLoginInput.value = ""
+            exibirAlerta('.alert-modal-login', "Login efetuado com sucesso!",['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+            ocultarModal('#modalLogin', 2000)
+            const loginLink = document.querySelector('#loginLink')
+            loginLink.innerHTML = "Logout"
+            const cadastrarFilmeButton = document.querySelector('#cadastrarFilmeButton')
+            cadastrarFilmeButton.disabled = false
+
+        }
+        catch(error){
+            exibirAlerta('.alert-modal-login', "Erro ao fazer login", ['show','alert-danger'], ['d-none', 'alert-success'], 2000)
+        }
+    }
+    else {
+        exibirAlerta('.alert-modal-login', 'Preencha todos os campos', ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
+    }
+
+}
